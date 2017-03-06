001dbdf29c7ae62c7962b97b86615543aa85ae77
Cclz <rex620620>
2017-03-06 14:23:00 +0800

    OnItemSelected

--- app/src/main/java/com/cclz/myapp_170306_3/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170306_3/MainActivity.java
@@ -2,12 +2,50 @@ package com.cclz.myapp_170306_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+implements View.OnClickListener{
+    Spinner spr;
+    Button btn;
+    TextView tv1,tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(Button) findViewById(R.id.button);
+        tv1=(TextView)findViewById(R.id.textView2);
+        tv2=(TextView)findViewById(R.id.textView3);
+        spr=(Spinner) findViewById(R.id.spinner);
+        btn.setOnClickListener(this);
+
+        spr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView spitv=(TextView) view;
+                tv1.setText(spitv.getText());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+
+                break;
+        }
     }
 }
